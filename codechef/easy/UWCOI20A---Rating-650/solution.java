7e7ca63953b8f5a47d1c00a4c9d2dcc53279245c
Bheemini Venkat Rohith <ugs24029_aid.venkatrohith@cbit.org.in>
2026-08-15 00:01:47 +0530

    ✅ UWCOI20A - Rating 650 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-08-14

--- codechef/easy/UWCOI20A---Rating-650/solution.java
+++ codechef/easy/UWCOI20A---Rating-650/solution.java
@@ -11,17 +11,16 @@ class Codechef
 		int T = sc.nextInt();
 		for(int i=0;i<T;i++){
 		    int N = sc.nextInt();
-		    String[] input = sc.nextLine().split(" ");
-		    int[] arr = new int[N];
 		    int max =Integer.MIN_VALUE;
+		    int[] arr=new int[N];
 		    for(int j=0;j<N;j++){
-		        arr[i]=Integer.parseInt(input[i]);
-		        if(arr[i]>max){
-		            max=arr[i];
+		        arr[j]=sc.nextInt();
+		        if(arr[j]>max){
+		            max=arr[j];
 		        }
 		    }
-		    System.out.print(max);
+		    System.out.println(max);
 		}
-
+        sc.close();
 	}
 }
