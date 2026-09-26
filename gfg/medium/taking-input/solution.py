019c7f765bcaf07660c6d0cbeba0638289ba5d3e
Bheemini Venkat Rohith <ugs24029_aid.venkatrohith@cbit.org.in>
2026-09-26 14:12:55 +0530

    ✅ taking-input | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-26

---
+++ gfg/medium/taking-input/solution.py
@@ -0,0 +1,15 @@
+import math
+
+# s to store string
+# n to store integer
+# f to store float
+# ff  # To Store floor of float variable f
+
+# code here
+s = input()
+n=input()
+f=float(input())
+ff=int(f/1 - f%1)
+print(s)
+print(n)
+print(ff)
